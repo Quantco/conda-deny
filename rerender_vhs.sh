7e176191931dc8d2a6ff90716b27f92a02ef838a
PaulKMueller <paul.mueller@quantco.com>
2024-10-22 14:39:26 +0200

    Reposition vhs script

--- rerender_vhs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# The following can be used to rerender the vhs demo tapes in .github/assets/demo
-
-# Make sure you are in an environment with vhs installed
-
-export PS1="> "
-vhs .github/assets/demo/demo-light.tape
-vhs .github/assets/demo/demo-dark.tape
