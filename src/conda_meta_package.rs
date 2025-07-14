4a8102f37a5169569eb3885eb4a2ea2e2951d351
PaulKMueller <paul.mueller@quantco.com>
2025-07-14 16:34:19 +0200

    Clean up abandoned struct

--- src/conda_meta_package.rs
+++ src/conda_meta_package.rs
@@ -1,5 +1,4 @@
 use core::panic;
-use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -8,22 +7,6 @@ struct CondaMetaPackage {
     version: String,
     build: String,
     platform: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct CondaMetaFile {
-    build: String,
-    build_number: i32,
-    depends: Vec<String>,
-    license: String,
-    license_family: Option<String>,
-    md5: String,
-    name: String,
-    noarch: Option<String>,
-    sha256: String,
-    size: i32,
-    subdir: String,
-    version: String,
 }
 
 impl CondaMetaPackage {
@@ -80,40 +63,6 @@ mod tests {
         assert_eq!(conda_meta_package.version, "1.0");
         assert_eq!(conda_meta_package.build, "pyhd8ed1ab_0");
         assert_eq!(conda_meta_package.platform, "noarch");
-    }
-
-    #[test]
-    fn test_conda_meta_file_creation() {
-        let conda_meta_file = CondaMetaFile {
-            build: "pyhd8ed1ab_0".to_string(),
-            build_number: 0,
-            depends: vec![
-                "python >=3.8,<3.9.0a0".to_string(),
-                "python_abi 3.8.* *_cp38".to_string(),
-            ],
-            license: "BSD-3-Clause".to_string(),
-            license_family: Some("BSD".to_string()),
-            md5: "d41d8cd98f00b204e9800998ecf8427e".to_string(),
-            name: "alabaster".to_string(),
-            noarch: Some("python".to_string()),
-            sha256: "d41d8cd98f00b204e9800998ecf8427e".to_string(),
-            size: 0,
-            subdir: "noarch".to_string(),
-            version: "0.7.16".to_string(),
-        };
-
-        assert_eq!(conda_meta_file.build, "pyhd8ed1ab_0");
-        assert_eq!(conda_meta_file.build_number, 0);
-        assert_eq!(conda_meta_file.depends.len(), 2);
-        assert_eq!(conda_meta_file.license, "BSD-3-Clause");
-        assert_eq!(conda_meta_file.license_family.unwrap(), "BSD");
-        assert_eq!(conda_meta_file.md5, "d41d8cd98f00b204e9800998ecf8427e");
-        assert_eq!(conda_meta_file.name, "alabaster");
-        assert_eq!(conda_meta_file.noarch.unwrap(), "python");
-        assert_eq!(conda_meta_file.sha256, "d41d8cd98f00b204e9800998ecf8427e");
-        assert_eq!(conda_meta_file.size, 0);
-        assert_eq!(conda_meta_file.subdir, "noarch");
-        assert_eq!(conda_meta_file.version, "0.7.16");
     }
 
     #[test]
